20021d9f13878ccbf4744dc3340f7aa451575b1f
Mike Beaumont <mjboamail@gmail.com>
2023-07-27 13:01:13 +0200

    refactor: put variables in e2e_env_vars

--- mk/run.mk
+++ mk/run.mk
@@ -5,6 +5,8 @@ E2E_ENV_VARS += KUMA_K8S_TYPE=k3d
 E2E_ENV_VARS += E2E_CONFIG_FILE="$(TOP)/test/cfg.yaml"
 E2E_ENV_VARS += KUMACTLBIN="$(KUMACTLBIN)"
 E2E_ENV_VARS += MESH_VERSION=$(MESH_VERSION)
+E2E_ENV_VARS += PERF_TEST_NUM_SERVICES=$${PERF_TEST_NUM_SERVICES:=5}
+E2E_ENV_VARS += PERF_TEST_STABILIZATION_SLEEP=$${PERF_TEST_STABILIZATION_SLEEP:=10s}
 
 .PHONY:
 fetch-mesh:
@@ -12,8 +14,6 @@ fetch-mesh:
 	[ -f $(KUMACTLBIN) ] || (cd build && curl -L https://docs.konghq.com/mesh/installer.sh | VERSION=$(MESH_VERSION) sh -)
 
 .PHONY: run
-run: export PERF_TEST_NUM_SERVICES ?= 5
-run: export PERF_TEST_STABILIZATION_SLEEP ?= 10s
 run: fetch-mesh
 	$(E2E_ENV_VARS) $(GINKGO) --json-report=raw-report.json ./test/...
-	jq '{Parameters: env | with_entries(select(.key | startswith("PERF_TEST"))), Suites: .}' raw-report.json > report.json
+	$(E2E_ENV_VARS) jq '{Parameters: env | with_entries(select(.key | startswith("PERF_TEST"))), Suites: .}' raw-report.json > report.json
