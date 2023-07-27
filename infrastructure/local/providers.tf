8f298463ea5aad7e374ee7a7f08e10327ac1c2ed
Mike Beaumont <mjboamail@gmail.com>
2023-07-27 12:47:07 +0200

    chore(terraform): upgrade package, add new hashes

--- infrastructure/local/providers.tf
+++ infrastructure/local/providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     docker = {
       source = "kreuzwerker/docker"
+      version = "3.0.2"
     }
     k3d = {
       source = "pvotal-tech/k3d"
-      version = "0.0.6"
+      version = "0.0.7"
     }
   }
   required_version = ">= 1.5.0"
