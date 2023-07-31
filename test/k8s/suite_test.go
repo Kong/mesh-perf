6da38b19cd13e2a6c89b3c924cf1ac8f89bca512
Mike Beaumont <mjboamail@gmail.com>
2023-07-31 23:46:47 +0200

    fix: switch to JSON, fix filenames

--- test/k8s/suite_test.go
+++ test/k8s/suite_test.go
@@ -1,6 +1,7 @@
 package k8s_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"path"
@@ -13,7 +14,6 @@ import (
 	obs "github.com/kumahq/kuma/test/framework/deployments/observability"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"gopkg.in/yaml.v2"
 
 	"github.com/kong/mesh-perf/test/framework"
 )
@@ -82,7 +82,7 @@ var _ = AfterSuite(func() {
 var _ = ReportAfterSuite("compile report", func(ginkgoReport Report) {
 	report := makeReport(ginkgoReport)
 
-	reportBytes, err := yaml.Marshal(report)
+	reportBytes, err := json.Marshal(report)
 	Expect(err).ToNot(HaveOccurred())
 
 	reportDir := os.Getenv("REPORT_DIR")
@@ -90,8 +90,8 @@ var _ = ReportAfterSuite("compile report", func(ginkgoReport Report) {
 		reportDir = "/tmp/perf-test-reports"
 	}
 	root := requireVar("TEST_ROOT")
-	relativeSuitePath := strings.TrimPrefix("/", strings.TrimPrefix(report.SuitePath, root))
-	fileName := fmt.Sprintf("%s.yaml", strings.ReplaceAll(relativeSuitePath, "/", "_"))
+	relativeSuitePath := strings.TrimPrefix(strings.TrimPrefix(report.SuitePath, root), "/")
+	fileName := fmt.Sprintf("%s.json", strings.ReplaceAll(relativeSuitePath, "/", "_"))
 	Expect(os.MkdirAll(reportDir, os.ModePerm))
 	Expect(os.WriteFile(path.Join(reportDir, fileName), reportBytes, 0666)).To(Succeed())
 })
