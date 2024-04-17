4c5ca4738019dd036e3e80bbeab14cda0f2c2afd
Lukasz Dziedziak <lukidzi@gmail.com>
2024-04-17 11:58:44 -0500

    chore(deps): upgrade k8s version
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -34,7 +34,7 @@ module "eks" {
   version = "20.8.5"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.27"
+  cluster_version = "1.29"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
