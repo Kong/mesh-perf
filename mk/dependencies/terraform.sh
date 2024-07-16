6f63cdd32c777ade15f59d87c545fef3802458c5
Lukasz Dziedziak <lukidzi@gmail.com>
2024-07-16 10:24:40 +0200

    chore(eks): separate eks and ecr creation (#197)
    Signed-off-by: Lukasz Dziedziak <lukidzi@gmail.com>

--- mk/dependencies/terraform.sh
+++ mk/dependencies/terraform.sh
@@ -3,7 +3,7 @@
 set -e
 
 OUTPUT_DIR=$1/bin
-VERSION="1.5.1"
+VERSION="1.5.7"
 curl --fail --location -s -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_${OS}_${ARCH}.zip
 unzip /tmp/terraform.zip -d $OUTPUT_DIR
 
