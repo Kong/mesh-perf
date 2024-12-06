f21fa26e339b6236356199b5c306d274d163cba0
Charly Molter <charly.molter@konghq.com>
2024-12-06 16:06:27 +0100

    fix terraform
    Signed-off-by: Charly Molter <charly.molter@konghq.com>

--- mk/dependencies/terraform.sh
+++ mk/dependencies/terraform.sh
@@ -5,5 +5,4 @@ set -e
 OUTPUT_DIR=$1/bin
 VERSION="1.5.7"
 curl --fail --location -s -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_${OS}_${ARCH}.zip
-unzip /tmp/terraform.zip -d $OUTPUT_DIR
-
+unzip -o /tmp/terraform.zip -d $OUTPUT_DIR
