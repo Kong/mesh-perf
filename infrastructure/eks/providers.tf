808b2d6fc5359ab60d031823430c9826258f04e6
Bart Smykla <bartek@smykla.com>
2025-01-30 08:26:17 +0100

    chore: try to change used token in helm provider
    Signed-off-by: Bart Smykla <bartek@smykla.com>

--- infrastructure/eks/providers.tf
+++ infrastructure/eks/providers.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mesh-perf-state"
-    key = "terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-west-1"
   }
   required_providers {
@@ -18,16 +18,21 @@ terraform {
   required_version = ">= 1.5.0"
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [
+    data.aws_eks_cluster.cluster
+  ]
+}
+
 provider "helm" {
   kubernetes {
-    host = module.eks.cluster_endpoint
+    host  = module.eks.cluster_endpoint
+    token = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-    }
   }
 }
