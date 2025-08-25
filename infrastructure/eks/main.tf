04c815849344fae65bd2ac23c8a14fd8b05a7a36
slonka <slonka@users.noreply.github.com>
2025-08-25 11:44:32 +0200

    fix(eks): set version explicitly
    Signed-off-by: slonka <slonka@users.noreply.github.com>

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -151,6 +151,7 @@ module "ecr" {
 
 module "ebs_csi_irsa_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.60.0"
 
   role_name             = "ebs-csi-${var.cluster_name}"
   attach_ebs_csi_policy = true
