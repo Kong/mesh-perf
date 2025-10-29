e43378ac7ed3dbd5f8a03884f801f897b3b02f1e
Marcin Skalski <skalskimarcin33@gmail.com>
2025-10-29 18:07:49 +0100

    feat(deps): move to mise

--- mk/dependencies/terraform.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-OUTPUT_DIR=$1/bin
-VERSION="1.5.7"
-curl --fail --location -s -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/${VERSION}/terraform_${VERSION}_${OS}_${ARCH}.zip
-unzip -o /tmp/terraform.zip -d $OUTPUT_DIR
--- mk/dev.mk
+++ mk/dev.mk
@@ -1,26 +1,13 @@
-TOOLS_DIR = $(TOP)/tools
-TOOLS_DEPS_DIRS=$(TOP)/mk/dependencies
-TOOLS_DEPS_LOCK_FILE=mk/dependencies/deps.lock
-TOOLS_MAKEFILE=$(TOP)/mk/dev.mk
-CI_TOOLS_DIR ?= ${HOME}/.mesh-perf-dev
-ifdef XDG_DATA_HOME
-	CI_TOOLS_DIR := ${XDG_DATA_HOME}/mesh-perf-dev
-endif
-CI_TOOLS_BIN_DIR=$(CI_TOOLS_DIR)/bin
-
-GINKGO=$(CI_TOOLS_BIN_DIR)/ginkgo
-
 GOOS := $(shell go env GOOS)
 GOARCH := $(shell go env GOARCH)
 
-TERRAFORM=$(CI_TOOLS_BIN_DIR)/terraform
-JSONNET=$(CI_TOOLS_BIN_DIR)/jsonnet
-JSONNET_BUNDLER=$(CI_TOOLS_BIN_DIR)/jb
+ifeq (,$(shell which mise))
+$(error "mise - https://github.com/jdx/mise - not found. Please install it.")
+endif
+MISE := $(shell which mise)
 
-.PHONY: dev/tools/ginkgo
-dev/tools/ginkgo:
-	GOBIN=${CI_TOOLS_BIN_DIR} go install github.com/onsi/ginkgo/v2/ginkgo@$$(go list -f '{{.Version}}' -m github.com/onsi/ginkgo/v2)
-
-.PHONY: dev/tools
-dev/tools: dev/tools/ginkgo
-	$(TOOLS_DIR)/dev/install-dev-tools.sh $(CI_TOOLS_BIN_DIR) $(CI_TOOLS_DIR) "$(TOOLS_DEPS_DIRS)" $(TOOLS_DEPS_LOCK_FILE) $(GOOS) $(GOARCH) $(TOOLS_MAKEFILE)
+GINKGO=$(shell $(MISE) which ginkgo)
+GO=$(shell $(MISE) which go)
+JSONNET=$(shell $(MISE) which jsonnet)
+JSONNET_BUNDLER=$(shell $(MISE) which jb)
+TERRAFORM=$(shell $(MISE) which terraform)
