fb886e2398f1312efc8807418d74c1fc3731e922
Marcin Skalski <skalskimarcin33@gmail.com>
2025-11-17 09:02:13 +0100

    fix init
    Signed-off-by: Marcin Skalski <skalskimarcin33@gmail.com>

--- infrastructure/eks/main.tf
+++ infrastructure/eks/main.tf
@@ -147,7 +147,7 @@ module "ecr" {
 }
 
 module "ebs_csi_irsa_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts"
   version = "6.2.3"
 
   role_name             = "ebs-csi-${var.cluster_name}"
